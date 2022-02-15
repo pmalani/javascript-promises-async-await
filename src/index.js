26b30837df7ff81e91b88d230f917aa932bb9fb9
praka <p@m.org>
2022-02-15 13:51:29 -0800

    moduel 4

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ getBooksAndMoviesPromise.then(results => {
 export function getBooksOrMovies() {
     return Promise.race([fetchBooks(), fetchMovies()])
         .then(results => results)
-        .catch(error => console.log("Error waiting for the promise race", error));
+        .catch(error => {
+            console.log("Error waiting for the promise race", error)
+        });
 }
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
